60388dbfcc28f00341c2ed7c86e68d5449148fcf
arthur <arthur@24b6.net>
2016-07-25 11:24:45 -0400

    Testing expansion

--- lib/jekyll_auth/jekyll_site.rb
+++ lib/jekyll_auth/jekyll_site.rb
@@ -1,7 +1,8 @@
 class JekyllAuth
   class JekyllSite < Sinatra::Base
     register Sinatra::Index
-    set :public_folder, File.expand_path("#{JekyllAuth.destination}", Dir.pwd)
+    destination = JekyllAuth.destination
+    set :public_folder, File.expand_path(destination, Dir.pwd)
     use_static_index 'index.html'
 
     # Rewrite calls to .html files if they exist.
