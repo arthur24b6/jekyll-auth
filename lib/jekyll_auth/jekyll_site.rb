db0db38f02452e209615417a56b6ad7c6274a4b5
arthur <arthur@24b6.net>
2016-07-24 21:31:09 -0400

    Adds a very rough rewriting of top level urls

--- lib/jekyll_auth/jekyll_site.rb
+++ lib/jekyll_auth/jekyll_site.rb
@@ -5,9 +5,19 @@ class JekyllAuth
     set :public_folder, File.expand_path(destination, Dir.pwd)
     use_static_index 'index.html'
 
+    # Rewrite calls to .html files if they exist.
+    get '/:path' do
+      file = File.join(File.expand_path(settings.public_folder, Dir.pwd), "#{params['path']}.html")
+      if File.exists?(file)
+        File.read(file)
+      else
+        not_found
+      end
+    end
+
     not_found do
       status 404
-      four_oh_four = File.expand_path(destination + '/404.html', Dir.pwd)
+      four_oh_four = File.expand_path(settings.public_folder + '/404.html', Dir.pwd)
       File.read(four_oh_four) if File.exist?(four_oh_four)
     end
   end
