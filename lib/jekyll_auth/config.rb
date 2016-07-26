2be39797156e060c23aac997f4bc53edc51a68f9
arthur <arthur@24b6.net>
2016-07-26 09:12:06 -0400

    Updating

--- lib/jekyll_auth/config.rb
+++ lib/jekyll_auth/config.rb
@@ -6,16 +6,12 @@ class JekyllAuth
   def self.jekyll_config
     @config ||= begin
       jekyll_config = YAML.safe_load_file(config_file)
-    rescue
-      {}
     end
   end
 
   def self.destination
     @config ||= begin
       JekyllAuth.jekyll_config['destination'] || '_site'
-    rescue
-      {}
     end
   end
 
