b302e8c8b6296f801731f8c6a8f0028cdaf1073c
arthur <arthur@24b6.net>
2016-07-26 09:04:35 -0400

    Updating

--- lib/jekyll_auth/jekyll_site.rb
+++ lib/jekyll_auth/jekyll_site.rb
@@ -2,7 +2,7 @@ class JekyllAuth
   class JekyllSite < Sinatra::Base
     register Sinatra::Index
     destination = JekyllAuth.destination
-    puts destination.inspect
+    puts JekyllAuth.jekyll_config.inspect
     set :public_folder, File.expand_path(destination, Dir.pwd)
     use_static_index 'index.html'
 
